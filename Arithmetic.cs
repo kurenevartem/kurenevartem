6d962ffc2c77f1ce082ca04dd3d9166030b9c6de
kurenevartem <kurenevartem@yandex.ru>
2022-10-27 01:43:29 +0800

    Арифметические операции
    Сложение, вычитание, умножение, деление, остаток от деления.

---
+++ Arithmetic.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"X+Y={x+y}\nX-Y={x-y}\nX*Y={x*y}\nX/Y={x/y}\nX%Y={x%y}");
