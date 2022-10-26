c3716d27669ab3e55286e5fe4d58726426b50db4
kurenevartem <kurenevartem@yandex.ru>
2022-10-27 01:44:57 +0800

    Двумерный массив
    Рандомное заполнение двумерного массива 10х10.

---
+++ Random.cs
@@ -0,0 +1,11 @@
+﻿int[,] nums = new int[10, 10];
+Random random = new Random();
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        nums[i, j] = random.Next();
+        Console.Write($"{nums[i, j]} \t");
+    }
+    Console.WriteLine();
+}
