1e93d407f70a15d8ef664d25c673a45563f583ed
kurenevartem <kurenevartem@yandex.ru>
2022-11-02 00:15:36 +0800

    Фибоначчи исправленное
    Вывод чисел Фибоначчи до заданной с выводом общей суммы этих чисел.

---
+++ Фибоначчи исправл.cs
@@ -0,0 +1,21 @@
+﻿﻿void Fibonachi(string[] args)
+{
+    Console.Write("Enter the Fibonacci number: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    int a = 0;
+    int b = 1;
+    int sum = 0;
+        for (; ; )
+        {
+            int f = a + b;
+            if (f > x)
+                break;
+            Console.Write($"{f}\t");
+            sum += f;
+            a = b;
+            b = f;
+        }
+    Console.WriteLine($"\nThe sum of the Fibonacci numbers: {sum}");
+}
+Fibonachi(args);
