f4febbf81acc069033302758d807d2f4948d11b4
kurenevartem <kurenevartem@yandex.ru>
2022-11-02 00:14:00 +0800

    Класс Person со свойствами Name и Age
    Массив из 10 из экземпляров класса с заполнением и выводом в консоль

---
+++ Name Age.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+
+class Person
+{
+    public string name;
+    public int age;
+    public Person () { name = "Default"; age = 0; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person[] newPerson = new Person[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            newPerson[i] = new Person();
+            Console.Write("Enter a name: ");
+            newPerson[i].name = Console.ReadLine();
+            Console.Write("Enter the age: ");
+            newPerson[i].age = Convert.ToInt32(Console.ReadLine());
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"Hi, my name is {newPerson[i].name}, i'm {newPerson[i].age} years old\n");
+        }
+    }
+}
