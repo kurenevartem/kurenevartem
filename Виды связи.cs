b3d5575cea3a3db3624a849a4d01b5e03c1f956a
kurenevartem <kurenevartem@yandex.ru>
2022-11-09 20:15:54 +0800

    Виды связи
    Ассоциация, агрегация и композиция

---
+++ Виды связи.cs
@@ -0,0 +1,38 @@
+﻿
+// Ассоциация
+class Microcontroller
+{ }
+class Modules
+{
+    public Microcontroller Processor_module { get; set; }
+    public Microcontroller Power_supply_unit { get; set; }
+    public Microcontroller Input_modules { get; set; }
+    public Microcontroller Output_modules { get; set; }
+
+}
+
+
+//Агрегация
+public abstract class Sensors
+{ }
+public class MFK1500
+{
+    Sensors sensors;
+    public MFK1500 (Sensors sens)
+    {
+        sensors = sens;
+    }
+}
+
+
+//Композиция
+public class Executive_and_regulating_devices
+{ }
+public class Siemens_simatic_s7
+{
+    Executive_and_regulating_devices devices;
+    public Siemens_simatic_s7()
+    {
+        devices = new Executive_and_regulating_devices();
+    }
+}
